3968c36999550bb7d11d514009658e978fbf6fc0
grim <grim@grim.grim>
2020-01-30 21:38:31 +1300

    Refactored.

--- README.md
+++ README.md
@@ -2,11 +2,9 @@
 
 Firefox addon that allows scrolling by clicking and dragging on the page.
 
-You can switch from this behaviour and normal click/drag behaviour by clicking on any text (mouse-down + mouse-up without any mouse movement in between). You can switch back into the dragging mode by clicking twice on any non-text (mouse-down + mouse-up + mouse-down without any mouse movement in between)
+In order to be able to do normal interaction like selecting text you can change between this addon's behaviour and default Firefox behaviour by clicking without moving the mouse for the entire click. Clicking on text will set to default Firefox behaviour and clicking textless space will set to this addon's behaviour.
 
-Clicking and dragging on any element while in dragging mode will ignore normal drag behaviour, allowing you to scroll without having to worry if you mouse-down'd on a link, image or text.
 
-Useful for scrolling, but still allowing you to double click text to start selecting text normally.
 
-https://addons.mozilla.org/en-US/developers/addon/drag-to-scroll/versions
+
 
--- content_script.js
+++ content_script.js
@@ -1,30 +1,41 @@
-var x , xStart , y , yStart;
-var multiplier = 8;
-var mode = "drag";
-function stop( e ) { e.stopPropagation(); e.preventDefault(); }
-function mouseMoved() { return ( x != xStart || y != yStart ) }
-function dragging( e ) {
-	window.scrollBy( ( x - e.screenX ) * multiplier , ( y - e.screenY ) * multiplier )
-	x = e.screenX ,	y = e.screenY;
+const multiplier = 8; // scroll this many pixels for each pixel worth of mouse movement.
+const IGNORE_TAGS = ["SELECT" , "AUDIO" , "VIDEO" , "INPUT" , "TEXTAREA"];
+let dragMode = true;
+let mouseMoved = false;
+const dragging = ( e ) => {
+	mouseMoved = true;
+	if ( dragMode ) {
+		window.scrollBy( e.movementX * multiplier * -1 , e.movementY * multiplier * -1 );
+	}
 }
-function mousedown( e ) {
-	if ( e.button != 0 ) return;
-	var setMode = ( x == e.screenX && y == e.screenY && e.explicitOriginalTarget.nodeType != 3) // these tests needs to be done now, but mode needs to be set later
-	x = xStart = e.screenX , y = yStart = e.screenY;
-	if ( ["select","audio","video","input","textarea"].includes( e.target.tagName.toLowerCase() ) ) return;
-	if ( setMode ) mode = "drag";
-	if ( mode == "drag" ) document.addEventListener( "mousemove" , dragging , true );
+const mousedown = ( e ) => {
+	if ( e.button == 0 && !IGNORE_TAGS.includes( e.target.tagName ) ) {
+		document.addEventListener( "mousemove" , dragging , true );
+		mouseMoved = window.getSelection().toString() != "" && e.target.tagName != "A"; // Assumes user is trying to deselect text instead of change modes. The mouse didn't move.
+	}
 }
-function mouseup( e ) {
-	document.removeEventListener( "mousemove" , dragging , true );
-	if ( !mouseMoved() && "a" != e.target.tagName.toLowerCase() ) {
-		if ( e.explicitOriginalTarget.nodeType == 3 || ["input","textarea"].includes( e.target.tagName.toLowerCase() ) ) { // nodeType 3 is TEXT_NODE
-			mode = "normal";
+const mouseup = ( e ) => {
+	if ( e.button == 0 ) {
+		document.removeEventListener( "mousemove" , dragging , true );
+		if ( !mouseMoved && e.target.tagName != "A" && !IGNORE_TAGS.includes( e.target.tagName ) ) { // <a> tag check for when clicking a link opens a new tab and you want to stay in drag mode in existing tab.
+			dragMode = e.explicitOriginalTarget.nodeType != 3; // 3 represents a text node.
 		}
 	}
 }
+const selectOrDragStart = ( e ) => {
+	if ( dragMode ) {
+		e.preventDefault();
+	}
+}
+const click = ( e ) => {
+	if ( mouseMoved && e.button == 0 ) {
+		e.stopPropagation();
+		e.preventDefault();
+	}
+	mouseMoved = false;
+}
 document.addEventListener( "mousedown" , mousedown , true );
 document.addEventListener( "mouseup" , mouseup , true );
-document.addEventListener( "selectstart" , ( e => { if ( mode == "drag" ) e.preventDefault() } ) );
-document.addEventListener( "dragstart" , ( e => { if ( mode == "drag" ) e.preventDefault() } ) );
-document.addEventListener( "click" , ( e => { if ( mouseMoved() && e.button == 0 ) stop( e ) } ) , true );
+document.addEventListener( "selectstart" , selectOrDragStart , true );
+document.addEventListener( "dragstart" , selectOrDragStart , true );
+document.addEventListener( "click" , click , true );
